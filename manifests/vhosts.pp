095f20b67a9b270a7ce003941019fd08e584d698
root <root@Master.4trifhadyawergoizhdiqh34le.bx.internal.cloudapp.net>
2020-09-03 02:22:37 +0000

    Added vhosts defined_type

---
+++ manifests/vhosts.pp
@@ -0,0 +1,38 @@
+# @summary
+#   Generated virtual hosts files
+#
+# @example
+#   apache::vhosts { 'namevar':
+#     port      => INT,
+#     subdomain => STR,
+#     admin     => STR,
+#     docroot   => STR,
+#   }
+define apache::vhosts (
+  Integer $port,
+  String[1] $subdomain,
+  String    $admin,
+  String[1] $docroot,
+) {
+  file { "${docroot}":
+    ensure => 'directory',
+    owner  => $apache::vhosts_owner,
+    group  => $apache::vhosts_group,
+  }
+
+  $vhosts_config_hash = {
+    'port'      => $port,
+    'subdomain' => $subdomain,
+    'admin'     => $admin,
+    'docroot'  => $docroot,
+  }
+
+  file { "${apache::vhosts_dir}/${subdomain}.conf":
+    ensure  => 'file',
+    owner   => $apache::vhosts_owner,
+    group   => $apache::vhosts_group,
+    mode    => '0644',
+    content => epp('apache/vhosts.conf.epp', $vhosts_config_hash),
+    notify  => Service['apache_service'],
+  }
+}
